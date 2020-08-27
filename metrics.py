4464605f49f20ece543b796c8a538774e92c3dd6
Lars-Roger Billingsø <larsroger@gmail.com>
2020-08-27 13:29:07 +0200

    Decouple metrics collection from scraping (#26)
    This commit decouples the collecting of metrics from the scraping
    perfomed by Prometheus. This is achieved by running a background
    thread wich collects the metrics from Jira and stores it in a global
    datastructure. When the root context is queried this datastructure is
    simply returned. The background job is, as of now, configured to run
    every 5 minutes.
    
    The metrics removed in previous commits are reinstated.
    
    Co-authored-by: @are3k
    Co-authored-by: @ArielKarlsen

--- metrics.py
+++ metrics.py
@@ -1,61 +1,52 @@
 import asyncio
 import logging
 import json
+import sys
+from apscheduler.schedulers.background import BackgroundScheduler
 from aiohttp import web
-from aiocache import cached
-from aiocache.serializers import JsonSerializer
 from jiracollector import JiraCollector
+from datetime import datetime
 
+cachedMetrics = ""
+serviceIsReady = False
 
-@cached(ttl=45, key="function_key", serializer=JsonSerializer())
-async def getJiraCollector():
+def collectJiraMetrics():
+    global cachedMetrics
+    global serviceIsReady
+    print("Running async")
+
     jiraCollector = JiraCollector()
-    return jiraCollector.collect()
-
-
-async def metrics(request):
-    metricsDict = await getJiraCollector()
+    metricsDict = jiraCollector.collect()
     metricsStr = str(metricsDict)
     metricsStrReplaced = metricsStr[1:-1].replace("'", "").replace(':', '').replace(",", "\n").replace(" j", "j") + "\n"
-    return web.Response(text=metricsStrReplaced)
+    cachedMetrics = metricsStrReplaced
+    serviceIsReady = True
 
-
-# It is also possible to cache the whole route, but for this you will need to
-# override `cached.get_from_cache` and regenerate the response since aiohttp
-# forbids reusing responses
-class CachedOverride(cached):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-
-    async def get_from_cache(self, key):
-        try:
-            value = await self.cache.get(key)
-            if type(value) == web.Response:
-                return web.Response(
-                    body=value.body,
-                    status=value.status,
-                    reason=value.reason,
-                    headers=value.headers,
-                )
-            return value
-        except Exception:
-            logging.exception("Couldn't retrieve %s, unexpected error", key)
+def metrics(request):
+    global cachedMetrics
+    return web.Response(text=cachedMetrics)
 
 def alive(request):
     return web.Response(status=200, text="OK")
 
 def ready(request):
-    ready = True
-    if ready:
+    global serviceIsReady
+    if serviceIsReady:
         return web.Response(status=200, text="OK")
     else:
         return web.Response(status=503, text="Not ready yet")
 
 
 if __name__ == "__main__":
+    # Create a background thread to collect metrics in a "cache"
+    scheduler = BackgroundScheduler()
+    job = scheduler.add_job(collectJiraMetrics, 'interval', minutes=5,max_instances=1,next_run_time=datetime.now())
+    scheduler.start()
+    
+    # Create a webapp to serve cached metrics on / endpoint
     app = web.Application()
     app.router.add_get('/', metrics)
     app.router.add_get('/health/ready', ready)
-    app.router.add_get('/health/alive', ready)
+    app.router.add_get('/health/alive', alive)
 
     web.run_app(app)
