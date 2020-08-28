157a7e06f8a0350e3ab7e9d881ccfe40dc5647b5
Lars-Roger Billingsø <lrb@ssb.no>
2020-08-28 13:43:45 +0200

    Introduce structured logging
    This commit will introduce structured logging i json-format as the
    logs will be collected in Kibana. Also added more logging statements
    and created two simple metrics for this application regarding the
    execution time and the total number of metrics collected for each
    run.
    
    (Still some refactoring to be done but this will be added in
    later commits/PRs)

--- jiracollector.py
+++ jiracollector.py
@@ -4,19 +4,22 @@ import google.auth
 from google.cloud import secretmanager
 from atlassian import Jira
 from urllib.error import HTTPError
+import logging
+logger = logging.getLogger()
 
 class JiraCollector(object):
     def __init__(self):
         self.api_key = self.getApiKey()
         self.jira_conn = self.getJiraConnection()
         self.result = {}
-
+        
     def getApiKey(self):
+        logger.info('Getting API-key')
         try:
             # Get credenetials from Workload Identity
             credentials = google.auth.default()
         except:
-            print("Could not get credentials from Google Cloud", file=sys.stderr)
+            logger.exception("Could not get credentials from Google Cloud")
         try:
             # Create client for secret manager
             client = secretmanager.SecretManagerServiceClient()
@@ -25,28 +28,31 @@ class JiraCollector(object):
             response = client.access_secret_version(name)
             apiKey = response.payload.data
         except:
-            print("Could not get apiKey for Google Secret Manager", file=sys.stderr)
+            logger.exception("Could not get apiKey for Google Secret Manager")
             apiKey = None
         return apiKey
     
     def getJiraConnection(self):
+        logger.info('Connecting to Jira')
         try:
             jira_conn = Jira(
             url='https://statistics-norway.atlassian.net',
             username='egk@ssb.no',
             password=self.api_key)
         except HTTPError:
-            print("Error in connection with Jira", file=sys.stderr)
+            logger.exception("Error in connection with Jira")
             jira_conn = None
         return jira_conn
 
     def queryJira(self, jql, limitResults='None'):
+        logger.debug('Running query : %s', jql)
         try:
             return self.jira_conn.jql(jql,limit=limitResults)['total']
         except HTTPError:
-            print("Error in connection with Jira", file=sys.stderr)
+            logger.exception("Error in connection with Jira")
 
     def collect(self):
+        logger.info("Creating jql from json")
         with open('metricdescriptions.json') as json_file:
             metricdescriptions = json.load(json_file)
         for metric in metricdescriptions['metrics']:
@@ -54,5 +60,6 @@ class JiraCollector(object):
                 key = metric['name'] + "{project_name=\"" + project['name'] + "\"}"
                 jql = "project = " + project['name'] + " AND " + metric['jql']
                 value = str(self.queryJira(jql, metric['limit']))
+                logger.debug("Got value: %s for metric: %s",value,key )
                 self.result[key] = value
         return self.result
--- metrics.py
+++ metrics.py
@@ -6,23 +6,45 @@ from apscheduler.schedulers.background import BackgroundScheduler
 from aiohttp import web
 from jiracollector import JiraCollector
 from datetime import datetime
+from pythonjsonlogger import jsonlogger
+import logging.config
+
+logging.config.fileConfig('logging.config', defaults=None, disable_existing_loggers=True)
+logger = logging.getLogger(__name__)
 
 cachedMetrics = ""
 serviceIsReady = False
-
 def collectJiraMetrics():
+    start = datetime.now()
+    logger.info('Start collecting metrics...')
     global cachedMetrics
     global serviceIsReady
-    print("Running async")
-
+    
     jiraCollector = JiraCollector()
     metricsDict = jiraCollector.collect()
+ 
+    logger.info("Converting metrics to strings")
     metricsStr = str(metricsDict)
     metricsStrReplaced = metricsStr[1:-1].replace("'", "").replace(':', '').replace(",", "\n").replace(" j", "j") + "\n"
+    end = datetime.now()    
+
+    # Logg and add metrics for this application
+    logger.info("Adding metrics for this application")
+    totalExecutionTime = abs((end - start).seconds)
+    totalNumberOfMetrics = len(metricsDict)
+    metricsStrReplaced = metricsStrReplaced+"jira_total_number_of_metrics "+str(totalNumberOfMetrics)+"\n"
+    metricsStrReplaced = metricsStrReplaced+"jira_total_execution_time_seconds "+str(totalExecutionTime)+"\n"
+    
     cachedMetrics = metricsStrReplaced
+    logger.info("% d metrics collected OK in %d seconds",totalNumberOfMetrics,totalExecutionTime)
+    
+
     serviceIsReady = True
 
+
+
 def metrics(request):
+    logger.info('Retrieving cached metrics')
     global cachedMetrics
     return web.Response(text=cachedMetrics)
 
@@ -38,11 +60,12 @@ def ready(request):
 
 
 if __name__ == "__main__":
+    logger.info("Starting app")
     # Create a background thread to collect metrics in a "cache"
     scheduler = BackgroundScheduler()
     job = scheduler.add_job(collectJiraMetrics, 'interval', minutes=15, max_instances=1, next_run_time=datetime.now())
     scheduler.start()
-    
+
     # Create a webapp to serve cached metrics on / endpoint
     app = web.Application()
     app.router.add_get('/', metrics)
