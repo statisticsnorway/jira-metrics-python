8642587fe47cb303bfe07935ce0de3b8027715fb
Lars-Roger Billingsø <lrb@ssb.no>
2020-08-31 13:14:38 +0200

    Introduce external properties
    This commit will extract some "hardcoded" values from the code
    and these will instead be read from a properties file. This is to
    prepare for future further externalization using Kubernetes config-
    maps (f.ex) so that properties may be updated without having to
    rebuild the docker-image

--- jiracollector.py
+++ jiracollector.py
@@ -4,8 +4,11 @@ import google.auth
 from google.cloud import secretmanager
 from atlassian import Jira
 from urllib.error import HTTPError
+import configparser
 import logging
 logger = logging.getLogger()
+config = configparser.ConfigParser()
+config.read('metrics.ini')
 
 class JiraCollector(object):
     def __init__(self):
@@ -24,10 +27,10 @@ class JiraCollector(object):
             # Create client for secret manager
             client = secretmanager.SecretManagerServiceClient()
             # Fetch secret from secret manager
-            name = client.secret_version_path('ssb-team-stratus', 'jira-api-key', 'latest')
+            name = client.secret_version_path(config.get('secrets','google_project_id'), config.get('secrets','name'), config.get('secrets','version'))
             response = client.access_secret_version(name)
             apiKey = response.payload.data
-        except:
+        except Exception as e:
             logger.exception("Could not get apiKey for Google Secret Manager")
             apiKey = None
         return apiKey
@@ -36,24 +39,17 @@ class JiraCollector(object):
         logger.info('Connecting to Jira')
         try:
             jira_conn = Jira(
-            url='https://statistics-norway.atlassian.net',
-            username='egk@ssb.no',
+            url=config.get('jira','url'),
+            username=config.get('jira','username'),
             password=self.api_key)
         except HTTPError:
             logger.exception("Error in connection with Jira")
             jira_conn = None
         return jira_conn
 
-    def queryJira(self, jql, limitResults='None'):
-        logger.debug('Running query : %s', jql)
-        try:
-            return self.jira_conn.jql(jql,limit=limitResults)['total']
-        except HTTPError:
-            logger.exception("Error in connection with Jira")
-
     def collect(self):
         logger.info("Creating jql from json")
-        with open('metricdescriptions.json') as json_file:
+        with open(config.get('jira','metrics_descriptions_file')) as json_file:
             metricdescriptions = json.load(json_file)
         for metric in metricdescriptions['metrics']:
             for project in metricdescriptions['projects']:
@@ -63,3 +59,10 @@ class JiraCollector(object):
                 logger.debug("Got value: %s for metric: %s",value,key )
                 self.result[key] = value
         return self.result
+
+    def queryJira(self, jql, limitResults='None'):
+        logger.debug('Running query : %s', jql)
+        try:
+            return self.jira_conn.jql(jql,limit=limitResults)['total']
+        except HTTPError:
+            logger.exception("Error in connection with Jira")        
--- metrics.py
+++ metrics.py
@@ -8,18 +8,27 @@ from jiracollector import JiraCollector
 from datetime import datetime
 from pythonjsonlogger import jsonlogger
 import logging.config
+import configparser
 
-logging.config.fileConfig('logging.config', defaults=None, disable_existing_loggers=True)
-logger = logging.getLogger(__name__)
+config = configparser.ConfigParser()
+config.read('metrics.ini')
+
+logging.config.fileConfig(config.get('logging'
+,'config_file'), defaults=None, disable_existing_loggers=True)
+
+logger = logging.getLogger()
 
 cachedMetrics = ""
 serviceIsReady = False
+
+# Collects metrics from Jira
 def collectJiraMetrics():
-    start = datetime.now()
-    logger.info('Start collecting metrics...')
     global cachedMetrics
     global serviceIsReady
-    
+
+    start = datetime.now()
+    logger.info('Start collecting metrics...')
+      
     jiraCollector = JiraCollector()
     metricsDict = jiraCollector.collect()
  
@@ -28,29 +37,31 @@ def collectJiraMetrics():
     metricsStrReplaced = metricsStr[1:-1].replace("'", "").replace(':', '').replace(",", "\n").replace(" j", "j") + "\n"
     end = datetime.now()    
 
-    # Logg and add metrics for this application
+    # Log and add metrics for this application
     logger.info("Adding metrics for this application")
     totalExecutionTime = abs((end - start).seconds)
     totalNumberOfMetrics = len(metricsDict)
     metricsStrReplaced = metricsStrReplaced+"jira_total_number_of_metrics "+str(totalNumberOfMetrics)+"\n"
     metricsStrReplaced = metricsStrReplaced+"jira_total_execution_time_seconds "+str(totalExecutionTime)+"\n"
     
+    # Update cached metrics
     cachedMetrics = metricsStrReplaced
     logger.info("% d metrics collected OK in %d seconds",totalNumberOfMetrics,totalExecutionTime)
     
-
     serviceIsReady = True
 
 
-
+# Serves the cached metrics at /
 def metrics(request):
     logger.info('Retrieving cached metrics')
     global cachedMetrics
     return web.Response(text=cachedMetrics)
 
+# Liveness
 def alive(request):
     return web.Response(status=200, text="OK")
 
+# Readiness
 def ready(request):
     global serviceIsReady
     if serviceIsReady:
@@ -63,7 +74,7 @@ if __name__ == "__main__":
     logger.info("Starting app")
     # Create a background thread to collect metrics in a "cache"
     scheduler = BackgroundScheduler()
-    job = scheduler.add_job(collectJiraMetrics, 'interval', minutes=15, max_instances=1, next_run_time=datetime.now())
+    job = scheduler.add_job(collectJiraMetrics, 'interval', minutes=config.getint('runtime_config','minutes_between_metrics_collection'), max_instances=1, next_run_time=datetime.now())
     scheduler.start()
 
     # Create a webapp to serve cached metrics on / endpoint
